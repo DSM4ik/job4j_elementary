c63bc57d5580acb2f389fd4c6289af153cc9654a
Denis Mitskevich <den.mitskevich@hotmail.com>
2022-05-15 21:39:42 +0300

    5. Конвертер валюты [#41583 #126510]

---
+++ src/main/java/ru/job4j/calculator/Converter.java
@@ -0,0 +1,20 @@
+package ru.job4j.calculator;
+
+public class Converter {
+    public static float rubleToEuro(float value) {
+        return value / 70;
+
+    }
+
+    public static float rubleToDollar(float value) {
+        return value / 60; /* формула перевода рублей в доллары. */
+
+    }
+
+    public static void main(String[] args) {
+        float euro = Converter.rubleToEuro(140);
+        float dollar = Converter.rubleToDollar(150);
+        System.out.println("140 rubles are " + euro + " euro.");
+        System.out.println("140 rubles are " + dollar + " euro.");
+    }
+}
